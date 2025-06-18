c615bc286d5b240a903fe4e461ed03b821336bdc
Nayeli De Jesus <nayelidjm142@gmail.com>
2025-06-17 22:21:37 -0700

    feat: retrieve user input in Flask without bugs

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__) 
 
 #home page
-@app.route("/")  
+@app.route("/", methods = ["POST", "GET"])  
 def index():
+    if request.method == "POST":
+        city = request.form["city"]
+        print(f"\nYou chose: {city}")
     return render_template("index.html")
 
 if __name__ in "__main__":

21d89f959f8f9cdaadd9e1b8c4555aa75bfe61d5
Nayeli De Jesus <nayelidjm142@gmail.com>
2025-06-18 11:15:07 -0700

    feat: added country code input and displayed weather details for city entered.

--- app.py
+++ app.py
@@ -16,20 +16,27 @@ print(f"this is the key {API_key}")
 #home page
 @app.route("/", methods = ["POST", "GET"])  
 def index():
+    weather_data = None
+    error = ""
     if request.method == "POST":
         city = request.form.get("city").strip()
+        country = request.form.get("country").strip()
         
-        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}"
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={API_key}"
         response = requests.get(url)
         data = response.json()
         if response.status_code == 200:
-            print(f"\nYou chose: {city}")
-            print(f"The weather at {city} is: {data}")
-            print(data)
+            weather_data = {
+                "country": data["sys"]["country"],
+                "city": data["name"],
+                "temperature": data["main"]["temp"],
+                "condition": data["weather"][0]["main"],
+                "description": data["weather"][0]["description"],
+            }
         else:
-            print(f"{city} not found.")
+            error = "Enter a valid city, and country code. Please try again."
         
-    return render_template("index.html")
+    return render_template("index.html", weather = weather_data, error = error)
 
 if __name__ in "__main__":
     app.run(debug=True, port = 5001)

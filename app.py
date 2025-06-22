22047613348d60fdd7fc32f0226502af86fc78da
Nayeli De Jesus <nayelidjm142@gmail.com>
2025-06-22 13:06:53 -0700

    feat: implement dog walk safety warning messages based on temperature and humidity

--- app.py
+++ app.py
@@ -20,8 +20,9 @@ def index():
     weather_data = None
     error = ""
     condition = ""
+    dog_warning = ""
             
-    city = "New York City"
+    city = "San Jose"
     country = "US"
                 
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={API_key}&units=imperial"
@@ -59,6 +60,24 @@ def index():
         sunrise_time = datetime.fromtimestamp(data['sys']['sunrise']).strftime('%I:%M %p')
         sunset_time = datetime.fromtimestamp(data['sys']['sunset']).strftime('%I:%M %p')
 
+        # if weather temp is above 85F = message -> be catuious dogs can overheat
+        # if weather temp is above 85 and humid is aove 60-75% then be cautious !!!
+        # if weather temp is 90 then it's too hot for most dogs
+        # if weaher is above 95 -> TOO hot to walk and unsafe!!!
+        weather_temp = round(data["main"]["temp"])
+        
+        humidity = data["main"]["humidity"]
+        if weather_temp <= 85 and humidity <= 60:
+            dog_warning = "Weather looks safe for your dog's walk. Enjoy your time outside!"
+        elif weather_temp > 85 and weather_temp <= 90 and humidity > 60 and humidity <= 70:
+            dog_warning = "It's getting warm. Keep walks shorts and stay hyrdated. Avoid midday heat."
+        elif weather_temp > 85 and humidity > 70:
+            dog_warning = "Warning: High heat and humidity. Limit activity and watch for signs of overheating."
+        elif weather_temp > 90 and weather_temp <= 95:
+            dog_warning = "It's very hot. Avoid long walks and stick to early morning or evenings."
+        elif weather_temp > 95:
+            dog_warning = "Danger: Too hot for dogs to walk safely. Please stay indoors."
+        
         if response.status_code == 200:
             weather_data = {
                 "country": data["sys"]["country"],
@@ -81,7 +100,7 @@ def index():
             
             error = "Enter a valid city, and country code. Please try again."
         
-    return render_template("index.html", weather = weather_data, error = error, condition = condition)
+    return render_template("index.html", dog_warning = dog_warning, weather = weather_data, error = error, condition = condition)
 
 if __name__ in "__main__":
     app.run(debug=True, port = 5001)

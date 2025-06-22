15342aacd2632363fd15a1f59de9754488a7c0cd
Nayeli De Jesus <nayelidjm142@gmail.com>
2025-06-21 22:37:35 -0700

    feat: add default city fallback and include humidity in weather data

--- app.py
+++ app.py
@@ -15,13 +15,38 @@ API_key = os.getenv("WEATHER_KEY")
 #home page
 @app.route("/", methods = ["POST", "GET"])  
 def index():
+   
     weather_data = None
     error = ""
     condition = ""
+            
+    city = "New York City"
+    country = "US"
+                
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={API_key}&units=imperial"
+
+    response = requests.get(url)
+    data = response.json()
+
+    if response.status_code == 200:
+        weather_data = {
+            "country": data["sys"]["country"],
+            "city": data["name"],
+            "temperature": round(data["main"]["temp"]),
+            "feels_temp": round(data["main"]["feels_like"]),
+            "humidity": data["main"]["humidity"],
+            "condition": data["weather"][0]["main"],
+            "description": data["weather"][0]["description"],
+            "icon": data["weather"][0]["icon"],
+            "wind": round(data["wind"]["speed"]),
+            "wind_gust": round(data["wind"].get("gust", 0))
+        }
+    condition = weather_data["condition"].lower()
+    
     if request.method == "POST":
-        city = request.form.get("city").strip()
-        state = request.form.get("state").strip()
-        country = request.form.get("country").strip()
+        city = request.form.get("city", "").strip()
+        state = request.form.get("state", "").strip()
+        country = request.form.get("country", "").strip()
         
         url = f"https://api.openweathermap.org/data/2.5/weather?q={city},{state},{country}&appid={API_key}&units=imperial"
         response = requests.get(url)
@@ -33,15 +58,18 @@ def index():
                 "city": data["name"],
                 "temperature": round(data["main"]["temp"]),
                 "feels_temp": round(data["main"]["feels_like"]),
+                "humidity": data["main"]["humidity"],
                 "condition": data["weather"][0]["main"],
                 "description": data["weather"][0]["description"],
                 "icon": data["weather"][0]["icon"],
                 "wind": round(data["wind"]["speed"]),
-                "wind_gust": round(data["wind"]["gust"])
+                "wind_gust": round(data["wind"].get("gust", 0))
             }
             condition = weather_data["condition"].lower()
      
         else:
+     
+            
             error = "Enter a valid city, and country code. Please try again."
         
     return render_template("index.html", weather = weather_data, error = error, condition = condition)
